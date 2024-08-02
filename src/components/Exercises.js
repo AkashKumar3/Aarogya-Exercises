493897839ade7bb9c6e415259da0239da1ac2ca7
AkashKumar3 <akashkumar8862825283@gmail.com>
2024-08-02 22:25:38 +0530

    change in exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -34,7 +34,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const paginate = (event, value) => {
     setCurrentPage(value);
 
-    window.scrollTo({ top: 1800, behavior: 'smooth' });
+    window.scrollTo({ top: 4800, behavior: 'smooth' });
   };
 
   if (!currentExercises.length) return <Loader />;
