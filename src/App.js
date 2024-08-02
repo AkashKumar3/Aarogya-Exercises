443a1d7600bf1ed8bbc4e0c4cd0f10c693591dd1
AkashKumar3 <akashkumar8862825283@gmail.com>
2024-08-02 21:13:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => (
     <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Aarogya-Exercises/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
       <Footer />
