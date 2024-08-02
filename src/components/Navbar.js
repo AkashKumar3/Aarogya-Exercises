bd2e6e39dd28ac4f1bf61f2d63373b995694d4bd
AkashKumar3 <akashkumar8862825283@gmail.com>
2024-08-02 23:19:50 +0530

    fix Home button and logo button error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import Logo from '../assets/images/aarogya.png';
 
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
-    <Link to="/">
+    <Link to="/Aarogya-Exercises">
       <img src={Logo} alt="logo" style={{ width: '60px', height: '60px', margin: '0px 20px' }} />
     </Link>
     <Stack
@@ -16,7 +16,7 @@ const Navbar = () => (
       fontSize="24px"
       alignItems="flex-end"
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #00ced1' }}>Home</Link>
+      <Link to="/Aarogya-Exercises" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #00ced1' }}>Home</Link>
       <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
     </Stack>
   </Stack>
